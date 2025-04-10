e7e8d9ead3c288c148b94fcdc67244a1a80e4b93
mahdiyar <smmhnp@gmail.com>
2025-04-10 19:40:25 +0330

    update navbar

--- mvcProject/app/View/base/nav.php
+++ mvcProject/app/View/base/nav.php
@@ -6,15 +6,29 @@
             </h3>
         </a>
         <nav class="nav nav-masthead justify-content-center float-md-end">
+            <?php 
+                $login = '';
+                $register = '';
+                if (isset($_GET['url'])){
+
+                    if ($_GET['url'] == 'users/login'){
+                        $login = 'active';
+                    }
+
+                    if($_GET['url'] == 'users/register'){
+                        $register = 'active';
+                    }
+                }
+            ?>
             <?php if (is_user_logged_in()): ?>
                 <a class="nav-link text-white"><?php echo $_SESSION['user_name']; ?></a>
                 <a class="nav-link" href="<?php echo URLROOT; ?>/users/logout">خروج</a>
             <?php else: ?>
-                <a class="nav-link fw-bold py-1 px-0 <?php echo ($_GET['url'] == 'users/login') ? 'active' : ''; ?>" aria-current="page" href="<?php echo URLROOT; ?>users/login">
+                <a class="nav-link fw-bold py-1 px-0 <?php echo $login; ?>" aria-current="page" href="<?php echo URLROOT; ?>users/login">
                     <i class="fas fa-home me-1"></i> 
                     ورود 
                 </a>
-                <a class="nav-link fw-bold py-1 px-0 <?php echo ($_GET['url'] == 'users/register') ? 'active' : ''; ?>" href="<?php echo URLROOT; ?>users/register">
+                <a class="nav-link fw-bold py-1 px-0 <?php echo $register; ?>" href="<?php echo URLROOT; ?>users/register">
                     <i class="fas fa-cog me-1"></i> 
                     عضویت 
                 </a>
